82275ba8e9ee1ff43fe4de6a8803c10b6336dd3e
Said Imanmagomedov <imanmagomedov.said@gmail.com>
2019-09-09 16:33:22 +0300

    add disabled for button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="ol-md-12">
+      <div class="col-md-12">
+        <h1>Default buttons</h1>
         <S4NButton small>Small button</S4NButton>
         <S4NButton medium>Medium button</S4NButton>
         <S4NButton large>Large button</S4NButton>
@@ -9,6 +10,7 @@
     </div>
     <div class="row">
       <div class="col-md-12">
+        <h1>Theme buttons</h1>
         <S4NButton primary>Primary</S4NButton>
         <S4NButton warning>Warning</S4NButton>
         <S4NButton success>Success</S4NButton>
@@ -16,6 +18,18 @@
         <S4NButton info>Info</S4NButton>
         <S4NButton dark>Dark</S4NButton>
         <S4NButton light>Light</S4NButton>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Disabled buttons</h1>
+        <S4NButton primary disabled>Primary</S4NButton>
+        <S4NButton warning disabled>Warning</S4NButton>
+        <S4NButton success disabled>Success</S4NButton>
+        <S4NButton error disabled>Error</S4NButton>
+        <S4NButton info disabled>Info</S4NButton>
+        <S4NButton dark disabled>Dark</S4NButton>
+        <S4NButton light disabled>Light</S4NButton>
       </div>
     </div>
   </div>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import 'normalize.css'
+import '@/assets/styles/index.scss'
 export { default as S4NButton } from './components/S4NButton/S4NButton.vue'
