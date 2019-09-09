0675e1bb6635abac4964d2f9a359e0dc60713553
Said Imanmagomedov <imanmagomedov.said@gmail.com>
2019-09-09 17:48:35 +0300

    create new component alert

---
+++ src/components/S4NAlert/S4NAlert.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    :class="getClasses"
+  >
+    <h4 :class="baseClass + '__header'" v-if="hasHeaderSlot">
+      <slot name="header"></slot>
+    </h4>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { BaseMixin } from '@/mixins/BaseMixin'
+
+export default {
+  name: 'S4NAlert',
+
+  mixins: [ BaseMixin ],
+
+  data: () => {
+    return {
+      baseClass: 's4n-alert'
+    }
+  },
+
+  computed: {
+    getClasses () {
+      let currentClasses = this.baseClass
+      // Color class
+      currentClasses += this.getColorClass
+      return currentClasses
+    },
+    hasHeaderSlot () {
+      return !!this.$slots.header
+    }
+  }
+}
+</script>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import 'normalize.css'
 import '@/assets/styles/index.scss'
+
 export { default as S4NButton } from './components/S4NButton/S4NButton.vue'
+export { default as S4NAlert } from './components/S4NAlert/S4NAlert.vue'
